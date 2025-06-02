a1f7f24d2df27cd864ae630acf7bc8bdb3b70622
gopikrishnagajam <gopi.gopikrishna204@gmail.com>
2025-06-02 16:38:03 -0500

    new

--- calorie_tracker/calorie_tracker/settings.py
+++ calorie_tracker/calorie_tracker/settings.py
@@ -127,5 +127,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ required for collectstatic
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
